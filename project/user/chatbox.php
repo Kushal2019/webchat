8e42d7e34846b97e4ab56aa1481188587710fa58
Kushal2019 <www.kushalmukherjee@gmail.com>
2020-06-25 22:23:02 +0530

    sss

--- project/user/chatbox.php
+++ project/user/chatbox.php
@@ -8,8 +8,8 @@
         <meta name="author" content="" />
         <title>Dashboard - SB Admin</title>
         <link href="../css/styles.css" rel="stylesheet" />
-        <link href="../css/usermyprofile.css" rel="stylesheet" />
-        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <link href="../css/chatbox.css" rel="stylesheet" />
+       
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
@@ -19,8 +19,80 @@
         ?>
      <div id="layoutSidenav_content">
     <main>
-    
-     </main>
+       
+<div class="card-body chat-care">
+                    <ul class="chat">
+                        <li class="agent clearfix">
+                            <span class="chat-img left clearfix mx-2">
+                                <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="Agent" class="img-circle" />
+                            </span>
+                            <div class="chat-body clearfix">
+                                <div class="header clearfix">
+                                    <strong class="primary-font">Jack Sparrow</strong> <small class="right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="admin clearfix">
+                            <span class="chat-img right clearfix  mx-2">
+                                <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="Admin" class="img-circle" />
+                            </span>
+                            <div class="chat-body clearfix">
+                                <div class="header clearfix">
+                                    <small class="left text-muted"><span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                    <strong class="right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="agent clearfix">
+                            <span class="chat-img left clearfix mx-2">
+                                <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="Agent" class="img-circle" />
+                            </span>
+                            <div class="chat-body clearfix">
+                                <div class="header clearfix">
+                                    <strong class="primary-font">Jack Sparrow</strong> <small class="right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="admin clearfix">
+                            <span class="chat-img right clearfix  mx-2">
+                                <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="Admin" class="img-circle" />
+                            </span>
+                            <div class="chat-body clearfix">
+                                <div class="header clearfix">
+                                    <small class="left text-muted"><span class="glyphicon glyphicon-time"></span>15 mins ago</small>
+                                    <strong class="right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    hfgjkhj
+
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-footer">
+                    <div class="input-group">
+                        <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary" id="btn-chat">
+                                Send</button>
+                        </span>
+                    </div>
+                </div>
+        
+    </main>
 
                <?php
             include("../pagesfile/footer.php");
@@ -31,6 +103,7 @@
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="../js/scripts.js"></script>
+        <script src="../js/chat.js"></script>
        
     </body>
 </html>
