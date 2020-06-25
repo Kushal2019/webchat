77e0897db4da18b258f4e07c204cb50ca54fb0d5
Kushal2019 <www.kushalmukherjee@gmail.com>
2020-06-25 16:45:17 +0530

    last update bye soumen

--- project/user/index.php
+++ project/user/index.php
@@ -22,12 +22,12 @@
     <div class="container emp-profile">
             <form method="post">
                 <div class="row">
-                    <div class="col-md-4">
+                    <div class="col-md-3">
                         <div class="profile-img">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt=""/>
-                            <div class="file btn btn-lg btn-primary">
+                            <div class="file btn btn-lg btn-primary" >
                                 Change Photo
-                                <input type="file" name="file"/>
+                                <input type="file" name="file" accept="" />
                             </div>
                         </div>
                     </div>
@@ -56,18 +56,7 @@
                 </div>
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="profile-work">
-                            <p>WORK LINK</p>
-                            <a href="">Website Link</a><br/>
-                            <a href="">Bootsnipp Profile</a><br/>
-                            <a href="">Bootply Profile</a>
-                            <p>SKILLS</p>
-                            <a href="">Web Designer</a><br/>
-                            <a href="">Web Developer</a><br/>
-                            <a href="">WordPress</a><br/>
-                            <a href="">WooCommerce</a><br/>
-                            <a href="">PHP, .Net</a><br/>
-                        </div>
+                        
                     </div>
                     <div class="col-md-8">
                         <div class="tab-content profile-tab" id="myTabContent">
