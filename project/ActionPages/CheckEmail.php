e2163fc60179d3cf409834d13908c6b4441eaa73
Kushal2019 <www.kushalmukherjee@gmail.com>
2020-06-25 12:10:59 +0530

    update by bhola

--- project/ActionPages/CheckEmail.php
+++ project/ActionPages/CheckEmail.php
@@ -1,20 +1,12 @@
 <?php
 session_start();
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
 ob_start();
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-
-// Load Composer's autoloader
 require '../mail/vendor/autoload.php';
-//include('connect.php');
-  //  print_r($value);
-// Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 $email=$_POST['id'];
-
 include('../connect.php');
 $sql="select * from user_detalis where email='$email'";
 $d=mysqli_fetch_array(mysqli_query($con,$sql));
@@ -26,21 +18,17 @@ $d=mysqli_fetch_array(mysqli_query($con,$sql));
  {
 
 try {
-    //Server settings
     $str=md5(microtime());;
 	$str=substr($str,0,6);
-	//echo $str; -------------------- verification code----------------
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'SMTP.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'bcaprogramup@gmail.com';                     // SMTP username
-    $mail->Password   = 'surojit007';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    //Recipients
-    $mail->setFrom('bcaprogramup@gmail.com', 'Surojit');
+    $mail->Username   = 'webchatphp@gmail.com';                     // SMTP username
+    $mail->Password   = 'webchat123';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; 
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for 
+    $mail->setFrom('webchatphp@gmail.com', 'Surojit');
     $mail->addAddress($email);     // Add a recipient
   
     $mail->isHTML(true);                                  // Set email format to HTML
