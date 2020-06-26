03dc0a83f182b73329e4db4c8c41067ade1907d5
Kushal2019 <www.kushalmukherjee@gmail.com>
2020-06-26 12:03:13 +0530

    make by babu

--- project/user/index.php
+++ project/user/index.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+    $val="";
+    if(isset($_GET['msg']))
+    {
+    $GLOBALS['val']=$_GET['msg'];
+    }
+
+?>
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -12,8 +20,19 @@
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
         <script type="text/javascript" src="../js/user.js"></script>
+        <script type="text/javascript">
+        function check_up_ero(msg)
+        {
+            if(msg=='1')
+            {
+               document.getElementById("msg1").innerHTML="update ";
+                $("#myModal2").modal();
+                return false;
+            }
+        }
+      </script>
     </head>
-    <body class="sb-nav-fixed">
+    <body class="sb-nav-fixed" onload="check_up_ero(<?=$val?>)">
         <?php
             include("../pagesfile/topnav.php");
             include("../pagesfile/sidetop.php");
@@ -21,7 +40,6 @@
      <div id="layoutSidenav_content">
     <main>
     <div class="container emp-profile">
-            <form method="post">
                 <div class="row">
                     <div class="col-md-3">
                         <div class="profile-img">
@@ -61,6 +79,7 @@
                     </div>
                     <div class="col-md-8">
                         <div class="tab-content profile-tab" id="myTabContent">
+                            <form method="post" action="../ActionPages/user_detalis_up.php">
                             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                                         
                                         <div class="row">
@@ -68,7 +87,7 @@
                                                 <label>Name</label>
                                             </div>
                                             <div class="col-md-6">
-                                                <p><input type="text" name="name" id="ip1" class="change" value="soumen" disabled></p>
+                                                <p><input type="text" name="name" id="ip1" class="change" value="<?=$data['name']?>" disabled></p>
                                             </div>
                                         </div>
                                         <div class="row">
@@ -76,7 +95,7 @@
                                                 <label>Email</label>
                                             </div>
                                             <div class="col-md-6">
-                                                <p><input type="email" name="email" value="soumendolui077@gmail.com" disabled></p>
+                                                <p><input type="email" name="email" value="<?=$data['email']?>" disabled></p>
                                             </div>
                                         </div>
                                         <div class="row">
@@ -84,7 +103,7 @@
                                                 <label>Phone</label>
                                             </div>
                                             <div class="col-md-6">
-                                                <p><input type="number" name="name" id="ip2" class="change" value="9733699773" disabled></p>
+                                                <p><input type="number" name="phone" id="ip2" class="change" value="<?=$data['mobile']?>" disabled></p>
                                             </div>
                                         </div>
                                         <div class="row">
@@ -92,7 +111,7 @@
                                                 <label>Profession</label>
                                             </div>
                                             <div class="col-md-6">
-                                                <p><input type="text" name="name" id="ip3" class="change" value="student" disabled></p>
+                                                <p><input type="text" name="pro" id="ip3" class="change" value="<?=$data['profession']?>" disabled></p>
                                             </div>
                                         </div>
                                          <div class="row">
@@ -100,10 +119,11 @@
                                                
                                             </div>
                                             <div class="col-md-6">
-                                                <p><input type="submit" name="ud" value="Update" id="update" style="display: none;"></p>
+                                                <p><input type="submit" name="up" value="Update" id="update" style="display: none;" onclick="return check1()"></p>
                                             </div>
                                         </div>
                             </div>
+                        </form>
                             <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
                                         <div class="row">
                                             <div class="col-md-6">
@@ -159,11 +179,27 @@
         </div>
      </main>
 
-               <?php
+        <?php
             include("../pagesfile/footer.php");
            
         ?>
-            </div>
+         <div class="modal fade" id="myModal2" role="dialog">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          
+          <h5 class="modal-title"><i class="fa fa-warning" style="font-size:20px;color:red"></i> Warning</h5>
+          <button type="button" class="close" data-dismiss="modal" id="bt1" style="color:red;">&times;</button>
+        </div>
+        <div class="modal-body">
+          <p id="msg1" style="color:red;">Plese Enter The First Name </p>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal" id="bt" onclick="javascript:document.getElementById('inputEmailAddress').focus();" style="color:red;">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
         
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
