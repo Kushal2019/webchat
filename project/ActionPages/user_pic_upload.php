91b3da346c5635d8d94bf3699f08391d40ca9fd5
Kushal2019 <www.kushalmukherjee@gmail.com>
2020-06-28 11:18:19 +0530

    ssss

--- project/ActionPages/user_pic_upload.php
+++ project/ActionPages/user_pic_upload.php
@@ -3,7 +3,13 @@
 	include("UserallData.php");
 	$email=$_SESSION['email'];
 	$data=user_detalis($email);
-	$filename=$data['id'].".jpg";
+	$img="../upload/user_pic/".$data['user_pic'];
+	if($data['user_pic']!="user_pic")
+	{
+		unlink($img);
+	}
+
+	$filename=$_FILES['file']['name'];
 	$tmp_name=$_FILES['file']['tmp_name'];
 	$location="../upload/user_pic/".$filename;
 	move_uploaded_file($tmp_name,$location);
--- project/user/profliepic.php
+++
@@ -1,8 +0,0 @@
- <?php 
-include('../connect.php');
-	require '../ActionPages/UserallData.php';
-	$email=$_SESSION['email'];
-	$data=user_detalis($email);
-
- ?>
-<img src="../upload/user_pic/<?=$data['user_pic']?>" alt=""/>                       
